230afe5fb5ad5ae8ea02d4dcf20a4c058ae40ca6
ivanh <rik.chanel@gmail.com>
2022-09-18 14:53:44 +0300

    new activity

--- app/src/androidTest/java/Comp.java
+++ app/src/androidTest/java/Comp.java
@@ -1,4 +1,31 @@
+import org.junit.Test;
+
 public class Comp {
- private int mouse, keyboard,headphones;
+ private int mouse, pubg,headphones;
+
+ public int getMouse() {
+  return mouse;
+ }
+
+ public int getPubg() {
+  return pubg;
+ }
+
+ public int getHeadphones() {
+  return headphones;
+ }
+
+ public void setMouse(int mouse) {
+  this.mouse = mouse;
+ }
+
+ public void setPubg(int pubg) {
+  this.pubg = pubg;
+ }
+
+ public void setHeadphones(int headphones) {
+  this.headphones = headphones;
+ }
+
 
 }
